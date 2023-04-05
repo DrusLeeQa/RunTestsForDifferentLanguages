eccc09ef81b6f9cf6b45dc7572a6630ef0763a04
DrusLeeQa <122824108+DrusLeeQa@users.noreply.github.com>
2023-04-05 22:54:14 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,6 +6,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_basket_button(browser):
     browser.get(link)
-    button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket123")
+    button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert button, "Нет такой кнопки"
     time.sleep(7)
